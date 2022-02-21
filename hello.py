027fe4ada2dec5188d0cd8ca8016b34f03a21c6c
Bruno Rocha <rochacbruno@gmail.com>
2022-02-21 11:07:34 +0000

    Refactor hello

--- hello.py
+++ hello.py
@@ -16,7 +16,7 @@ Execução:
     ou
     ./hello.py
 """
-__version__ = "0.0.1"
+__version__ = "0.1.2"
 __author__ = "Bruno Rocha"
 __license__ = "Unlicense"
 
@@ -24,15 +24,12 @@ import os
 
 current_language = os.getenv("LANG", "en_US")[:5]
 
-msg = "Hello, World!"
+msg = {
+   "en_US": "Hello, World!",
+   "pt_BR": "Olá, Mundo!",
+   "it_IT": "Ciao, Mondo!",
+   "es_ES": "Hola, Mundo!",
+   "fr_FR": "Bonjour, Monde!", 
+}
 
-if current_language == "pt_BR":
-    msg = "Olá, Mundo!"
-elif current_language == "it_IT":
-    msg = "Ciao, Mondo!"
-elif current_language == "es_SP":
-    msg = "Hola, Mundo!"
-elif current_language == "fr_FR":
-    msg = "Bonjour Monde"
-
-print(msg)
+print(msg[current_language])
