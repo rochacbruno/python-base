fced84569cdcdf1e72dc9ad8a335ad431b1876ba
Bruno Rocha <rochacbruno@gmail.com>
2022-02-23 15:44:01 +0000

    stream handler logs

--- hello.py
+++ hello.py
@@ -26,6 +26,18 @@ __license__ = "Unlicense"
 
 import os
 import sys
+import logging
+
+log_level = os.getenv("LOG_LEVEL", "WARNING").upper()
+log = logging.Logger("bruno", log_level)
+ch = logging.StreamHandler()
+ch.setLevel(log_level)
+fmt = logging.Formatter(
+    '%(asctime)s  %(name)s  %(levelname)s '
+    'l:%(lineno)d f:%(filename)s: %(message)s'
+)
+ch.setFormatter(fmt)
+log.addHandler(ch)
 
 arguments = {"lang": None, "count": 1}
 
@@ -33,11 +45,11 @@ for arg in sys.argv[1:]:
     try:
         key, value = arg.split("=")
     except ValueError as e:
-        # TODO: Logging
-        print(f"[ERROR] {str(e)}")
-        print("You need to use `=`")
-        print(f"You passed {arg}")
-        print("try with --key=value")
+        log.error(
+            "You need to use `=`, you passed %s, try --key=value: %s",
+            arg,
+            str(e)
+        )
         sys.exit(1)
         
     key = key.lstrip("-").strip()
