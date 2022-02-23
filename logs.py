2faeaf990e5fbbb3a82ef3ffd83f6f326eeec35b
Bruno Rocha <rochacbruno@gmail.com>
2022-02-23 15:43:47 +0000

    stream handler logs

--- logs.py
+++ logs.py
@@ -2,20 +2,29 @@
 
 import os
 import logging
+from logging import handlers
 
 # BOILERPLATE
 # TODO: usar função
 # TODO: usar lib (loguru)
 log_level = os.getenv("LOG_LEVEL", "WARNING").upper()
 log = logging.Logger("bruno", log_level)
-ch = logging.StreamHandler()  # Console/terminal/stderr
-ch.setLevel(log_level)
+#ch = logging.StreamHandler()  # Console/terminal/stderr
+#ch.setLevel(log_level)
+fh = handlers.RotatingFileHandler(
+   "meulog.log", 
+   maxBytes=300, # 10**6
+   backupCount=10,
+)
+fh.setLevel(log_level)
 fmt = logging.Formatter(
     '%(asctime)s  %(name)s  %(levelname)s '
     'l:%(lineno)d f:%(filename)s: %(message)s'
 )
-ch.setFormatter(fmt)
-log.addHandler(ch)
+# ch.setFormatter(fmt)
+fh.setFormatter(fmt)
+#log.addHandler(ch)
+log.addHandler(fh)
 
 
 """
