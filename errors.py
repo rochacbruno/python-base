77202114e488a7dcf74ef5cba24e121db5bec790
Bruno Rocha <rochacbruno@gmail.com>
2022-03-07 11:12:16 +0000

    retry com for

--- errors.py
+++ errors.py
@@ -1,23 +1,29 @@
 #!/usr/bin/env python3
 import os
 import sys
+import time
+import logging
+
+log = logging.Logger("errors")
+
 
 # EAFP - Easy to ASk Forgiveness than permission
 # (É mais fácil pedir perdão do que permissão)
 
-try:
-    names = open("names.txt").readlines()  # FileNotFoundError
-except FileNotFoundError as e:
-    print(f"{str(e)}.")
-    sys.exit(1)
-    # TODO: Usar retry
-else:
-    print("Sucesso!!!")
-finally:
-    print("Execute isso sempre!")
+def try_to_open_a_file(filepath, retry=1) -> list:
+    """Tries to open a file, if error, retries n times."""
+    for attempt in range(1, retry + 1):
+        try:
+            return open(filepath).readlines()  # FileNotFoundError
+        except FileNotFoundError as e:
+            log.error("ERRO: %s", e)
+            time.sleep(2)
+        else:
+            print("Sucesso!!!")
+        finally:
+            print("Execute isso sempre!")
+    return []
 
-try:
-    print(names[2])
-except:
-    print("[Error] Missing name in the list")
-    sys.exit(1)
+
+for line in try_to_open_a_file("names.txt", retry=5):
+    print(line)
